8b51055fe5bb8d410b479eafdcde3373b7856ddd
Silvestre <zesilvestre2@gmail.com>
2026-06-10 12:49:04 -0300

    feat: Atualização da versão para 5.1.0 nos arquivos package.json, package-lock.json e na configuração de download para estar em conformidade com a versão mais recente.

--- src/app/shared/config/download.config.ts
+++ src/app/shared/config/download.config.ts
@@ -3,6 +3,6 @@ export const DOWNLOAD_CONFIG = {
   windowsX64Url:
     'https://github.com/ZeSilvestre/UtiliStudy-releases/releases/latest/download/UtiliStudy-Setup.exe',
   /** Manter alinhado com UtiliStudy/package.json a cada release */
-  version: '5.0.5',
+  version: '5.1.0',
   platform: 'Windows x64',
 };
